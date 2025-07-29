90f278bd5d4316556ed4a632e37a12fea6addd46
vinit234 <vinitv275@gmail.com>
2025-07-29 22:06:22 +0530

    some changes

--- src/main/java/bajaj/demo/DemoApplication.java
+++ src/main/java/bajaj/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package bajaj.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class DemoApplication {
